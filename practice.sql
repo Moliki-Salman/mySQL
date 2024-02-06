4fa6d414fd047b7a860b7ef5ffaf89acee1982d6
Moliki-Salman <salmanameenat@gmail.com>
2024-02-06 23:06:38 +0100

    order table created

--- practice.sql
+++ practice.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE Practice
+
 use practice
 
 create table customers(
@@ -6,4 +8,13 @@ first_name  VARCHAR(40),
 last_name  VARCHAR(40),
 email VARCHAR(40),
 phone_no INT(16)
+);
+
+DESCRIBE customers;
+
+CREATE TABLE orders (
+  `order_id` INT PRIMARY KEY NOT NULL  AUTO_INCREMENT,
+  `order_date` date,
+  `cus_id_fk` INT,
+CONSTRAINT customer_check FOREIGN KEY (cus_id_fk) REFERENCES customers(cus_id)
 );
