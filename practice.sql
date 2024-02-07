10ec7ef21e78d113f872ee38e0a2d424e1bd3097
Moliki-Salman <salmanameenat@gmail.com>
2024-02-08 00:16:18 +0100

    tables created

--- practice.sql
+++ practice.sql
@@ -16,5 +16,40 @@ CREATE TABLE orders (
   `order_id` INT PRIMARY KEY NOT NULL  AUTO_INCREMENT,
   `order_date` date,
   `cus_id_fk` INT,
+--contraint is used to apply rules to a,table. it ensures accuracy of the data in the tables--
 CONSTRAINT customer_check FOREIGN KEY (cus_id_fk) REFERENCES customers(cus_id)
+);
+
+DESCRIBE orders
+
+INSERT INTO customers(first_name, last_name, email, phone_no)
+VALUES ('Tbi', 'Bamidele', 'bams@gmail.com', '+2349087678'),
+('Oluwatosin', 'Bayode', 'tosin@gmail.com', '+2349077875'),
+('Oluchi', 'Joseph', 'oluchi@gmail.com', '+23490976437')
+
+CREATE TABLE `categories` (
+  `category_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `category_name`VARCHAR(250)
+);
+
+
+CREATE TABLE `products` (
+  `product_id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+  `product_Name` VARCHAR(250),
+  `category_id_fk` INT,
+  `prices` DECIMAL,
+  `stockQuantity`DECIMAL,
+-- CHECK is used to ensures that the values in a column satisfies a specific condition
+CONSTRAINT stock_check CHECK (`stockQuantity` >= 0)
+)
+
+
+CREATE TABLE `orderDetails` (
+  `orderDetails_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id_fk` INT,
+  `product_id_fk` INT,
+  `quantity` INT,
+  `totalPrice` DECIMAL,
+-- it is important to refrences the name of the table b4 stating the column eg products(product_id)
+  CONSTRAINT available_prod FOREIGN KEY (product_id_fk) REFERENCES products(product_id)
 );
